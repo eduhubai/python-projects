38f7c0e74014eb209efc3dcf338ddcb0429b41c1
eduhubai <165917035+eduhubai@users.noreply.github.com>
2024-04-03 17:24:15 +0200

    Update Challenge_5_Rock_Paper_Scissors.py

--- Challenge_5_Rock_Paper_Scissors.py
+++ Challenge_5_Rock_Paper_Scissors.py
@@ -1,37 +1,40 @@
 import random
+
 computer_score = 0
 human_score = 0
 number_of_ties = 0
 keep_playing = True
 
 valid_choices = ["rock", "paper", "scissors"]
-while True:
-    human_choice = input('enter human choice (rock, paper, scissors): ')
-    print('human chooses ' + human_choice)
-    if human_choice.lower() in valid_choices:  # Check for lowercase too
-        break  # Exit the loop if valid choice is entered
-    else:
-        print("Invalid choice. Please enter rock, paper, or scissors.")
 
-    computer_choice = random.choice(
-        ['rock', 'paper', 'scissors'])  # change this line
-    print('the computer chooses ' + computer_choice)
+while keep_playing:
+    while True:  # This loop ensures that the user inputs a valid choice
+        human_choice = input('Enter human choice (rock, paper, scissors): ').lower()  # Automatically converts input to lowercase
+        print('Human chooses ' + human_choice)
+        if human_choice in valid_choices:
+            break  # Exit the loop if valid choice is entered
+        else:
+            print("Invalid choice. Please enter rock, paper, or scissors.")
 
-    if (human_choice == computer_choice):
+    computer_choice = random.choice(valid_choices)  # Generate computer's choice
+    print('The computer chooses ' + computer_choice)
+
+    if human_choice == computer_choice:
         number_of_ties += 1
-        print('tie')
-    elif ((human_choice == 'rock' and computer_choice == 'paper') or (human_choice == 'paper' and computer_choice == 'scissors') or (human_choice == 'scissors' and computer_choice == 'rock')):
-        computer_score += 1
-        print('computer wins')
-    else:
+        print('Tie')
+    elif (human_choice == 'rock' and computer_choice == 'scissors') or (human_choice == 'paper' and computer_choice == 'rock') or (human_choice == 'scissors' and computer_choice == 'paper'):
         human_score += 1
-        print('human wins')
-    print("Do you want to play again?")
+        print('Human wins')
+    else:
+        computer_score += 1
+        print('Computer wins')
 
-    answer = input()
-    if answer == "no":
+    print("Do you want to play again? (yes/no)")
+    answer = input().lower()
+    if answer != "yes":
         keep_playing = False
-    print("Thanks for playing!")
-print('computer score: ' + str(computer_score))
-print('human score: ' + str(human_score))
-print('ties: ' + str(number_of_ties))
+
+print("Thanks for playing!")
+print('Computer score: ' + str(computer_score))
+print('Human score: ' + str(human_score))
+print('Ties: ' + str(number_of_ties))
